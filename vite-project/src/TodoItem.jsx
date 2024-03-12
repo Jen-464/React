5a47d990f199a6092ce5c73c045eb37eb8435f22
Jen-464 <jenlopez464@gmail.com>
2024-03-11 18:51:02 -0700

    completed the styling of the todo items

--- vite-project/src/TodoItem.jsx
+++ vite-project/src/TodoItem.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types';
 
 export default function TodoItem({ id, title, completed, toggleTodo, deleteTodo }) {
+    const handleDeleteKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          deleteTodo(id);
+        }
+      };
+
     return (
         <li>
             <label>
-                <input type="checkbox" checked={completed}
-                    onChange={e => toggleTodo(id, e.target.checked)} />
-                {title}
+                {/* <img src="../assets/icon-check.svg" alt="Cross Icon" className="cross"></img> */}
+                <input type="checkbox" checked={completed} onChange={e => toggleTodo(id, e.target.checked)} />
+                <span id="todoItem">{title}</span>
             </label>
             {/* eslint-disable-next-line no-unused-vars */}
-            <button onClick={e => deleteTodo(id)} className="btn btn-danger"> Delete </button>
+            <span onClick={e => deleteTodo(id)} onKeyDown={handleDeleteKeyDown} className="btn" tabIndex={0}> 
+            <img src="../assets/icon-cross.svg" alt="Cross Icon" className="cross"></img>
+            </span>
         </li>
     )
 }
